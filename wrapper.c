2291958dc4ebdd4236fb0a32b06a27fe4fd04aa0
Richard W.M. Jones <rjones@redhat.com>
2020-08-18 19:02:55 +0100

    wrapper: Port the wrapper to run on Windows.
    This also defines SOEXT as the extension of shared objects (ie. "so"
    or "dll"), and uses it everywhere.  I assumed this must already be
    defined by either autoconf or mingw (like OBJEXT) but I cannot find
    anything except in glib (G_MODULE_SUFFIX).
    
    Thanks: Zebediah Figura for helping out with exec vs spawn on Windows.

--- wrapper.c
+++ wrapper.c
@@ -72,6 +72,7 @@
 #include <time.h>
 
 #include "options.h"
+#include "windows-compat.h"
 #include "utils.h"
 
 /* Construct an array of parameters passed through to real nbdkit. */
@@ -141,6 +142,7 @@ main (int argc, char *argv[])
   char ts[32];
   int r;
 
+#ifndef WIN32
   /* If NBDKIT_VALGRIND=1 is set in the environment, then we run the
    * program under valgrind.  This is used by the tests.  Similarly if
    * NBDKIT_GDB=1 is set, we run the program under GDB, useful during
@@ -167,11 +169,17 @@ main (int argc, char *argv[])
       passthru ("--args");
     }
   }
+#endif
 
   /* As well as being required by nbdkit, this is also needed for
    * plugins written in OCaml.
    */
-  s = getenv ("LD_LIBRARY_PATH");
+#ifndef WIN32
+#define LD_LIBRARY_PATH "LD_LIBRARY_PATH"
+#else
+#define LD_LIBRARY_PATH "PATH"
+#endif
+  s = getenv (LD_LIBRARY_PATH);
   if (s)
     r = asprintf (&s, "%s/server/.libs:%s/plugins/ocaml/.libs:%s",
                   builddir, builddir, s);
@@ -182,7 +190,7 @@ main (int argc, char *argv[])
     perror ("asprintf");
     exit (EXIT_FAILURE);
   }
-  setenv ("LD_LIBRARY_PATH", s, 1);
+  setenv (LD_LIBRARY_PATH, s, 1);
   free (s);
   s = getenv ("LIBRARY_PATH");
   if (s)
@@ -197,7 +205,7 @@ main (int argc, char *argv[])
   free (s);
 
   /* Absolute path of the real nbdkit command. */
-  passthru_format ("%s/server/nbdkit", builddir);
+  passthru_format ("%s/server/nbdkit" EXEEXT, builddir);
 
   /* Option parsing.  We don't really parse options here.  We are only
    * interested in which options have arguments and which need
@@ -229,7 +237,8 @@ main (int argc, char *argv[])
     /* Filters can be rewritten if they are a short name. */
     else if (c == FILTER_OPTION) {
       if (is_short_name (optarg))
-        passthru_format ("--filter=%s/filters/%s/.libs/nbdkit-%s-filter.so",
+        passthru_format ("--filter="
+                         "%s/filters/%s/.libs/nbdkit-%s-filter." SOEXT,
                          builddir, optarg, optarg);
       else
         passthru_format ("--filter=%s", optarg);
@@ -262,13 +271,13 @@ main (int argc, char *argv[])
     if (is_short_name (argv[optind])) {
       /* Special plugins written in Perl. */
       if (is_perl_plugin (argv[optind])) {
-        passthru_format ("%s/plugins/perl/.libs/nbdkit-perl-plugin.so",
+        passthru_format ("%s/plugins/perl/.libs/nbdkit-perl-plugin." SOEXT,
                          builddir);
         passthru_format ("%s/plugins/%s/nbdkit-%s-plugin",
                          builddir, argv[optind], argv[optind]);
       }
       else {
-        passthru_format ("%s/plugins/%s/.libs/nbdkit-%s-plugin.so",
+        passthru_format ("%s/plugins/%s/.libs/nbdkit-%s-plugin." SOEXT,
                          builddir, argv[optind], argv[optind]);
       }
       ++optind;
@@ -299,7 +308,16 @@ main (int argc, char *argv[])
   setenv ("MALLOC_PERTURB_", ts, 0);
 
   /* Run the final command. */
+#ifndef WIN32
   execvp (cmd[0], (char **) cmd);
   perror (cmd[0]);
   exit (EXIT_FAILURE);
+#else /* WIN32 */
+  r = _spawnvp (_P_WAIT, cmd[0], cmd);
+  if (r == -1) {
+    perror (cmd[0]);
+    exit (EXIT_FAILURE);
+  }
+  exit (r == 0 ? EXIT_SUCCESS : EXIT_FAILURE);
+#endif /* WIN32 */
 }
